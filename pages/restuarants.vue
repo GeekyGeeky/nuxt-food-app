eaa8e4a4ed3405b9528fa34d8764e209eb8bac9e
geekygeeky <tobyolushola@gmail.com>
2021-07-29 07:01:06 +0100

    Initial commit

---
+++ pages/restuarants.vue
@@ -0,0 +1,21 @@
+<template>
+	<main class="container restuarant">
+		<!-- <app-header /> -->
+    <app-restuarant-info />
+	</main>
+</template>
+
+<script>
+ // import AppHeader from "@/components/AppHeader.vue";
+ import AppRestuarantInfo from "@/components/AppRestuarantInfo.vue";
+
+ export default{
+  components: {
+    // AppHeader,
+    AppRestuarantInfo,
+  }
+ }
+</script>
+
+<style>
+</style>
